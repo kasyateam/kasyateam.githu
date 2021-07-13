61793f77b549c4c2d0bf9f1a4bc954bf3ac72261
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-07-13 14:43:21 +0600

    Add files via upload

---
+++ proxy/knad.js
@@ -0,0 +1,10 @@
+function kasyanadzor() {
+  var input = null;
+  input = document.getElementById('site').value;
+  if (input == 'vk.com'){
+   document.getElementById('open').src = '/proxy/kasyanadzor.html'
+   document.getElementById('site').value = ''
+}else if (input == 'ok.ru'){
+   document.getElementById('open').src = '/proxy/kasyanadzor.html'
+   document.getElementById('site').value = ''
+}else{}}
---
+++ why/lib/whycheck.js
@@ -0,0 +1,5 @@
+var kasyateam = 'http://mini.s-shot.ru/1280x768/8000/png/?'
+function check () {
+var img = document.getElementById('view');
+img.innerHTML = '<img id="check" style="width: 100%; height: 100%;" src="'+kasyateam+document.getElementById("check").value+'"></div>'
+}
