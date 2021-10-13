286e0ebccfdb778f77e9cc79423c18f3802a2fc7
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-10-13 23:16:49 +0600

    Update code.js

--- m/code.js
+++ m/code.js
@@ -1,12 +1,12 @@
-if (window.location.pathname = '/index'){
+if (window.location.pathname == '/index'){
     document.title = 'K.Feed | Главная';
-}else if(window.location.pathname = '/feed'){
+}else if(window.location.pathname == '/feed'){
     document.title = 'K.Feed | Новости';
-}else if(window.location.pathname = '/k-feed'){
+}else if(window.location.pathname == '/k-feed'){
     document.title = 'K.Feed';
-}else if(window.location.pathname = '/kasya-id'){
+}else if(window.location.pathname == '/kasya-id'){
     document.title = 'K.Feed | Kasya ID';
-}else if(window.location.pathname = '/kmp'){
+}else if(window.location.pathname == '/kmp'){
     document.title = 'K.Feed | Курсы';
 }else{
 
