ca8eb6f7590dd6972960aec1b8524bf394c62e2e
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-10-15 00:29:33 +0600

    Update kasya.js

--- assets/kasyateam/js/kasya.js
+++ assets/kasyateam/js/kasya.js
@@ -1,24 +1,3 @@
-/*Kasya. Время работы
-var date = new Date(); 
-      var hour = date.getUTCHours();
-      if (hour>=18 && hour<19) online = "online";
-      else { if (hour>=19 && hour<24) online = "online";
-      else { if (hour>=0 && hour<1) online = "server";
-      else {  if (hour>=1&& hour<18) online = "online"; 
-      }
-   }
-}
-
-//Visual clearance 
-if (online == "online") {
-}if (online == "offline") { 
-window.location.href = "/offline";
-} else if (online == "server") {
-window.location.href = "/server";
-} else {
-}
-/
-*/
 //Plagiarism detection testing
 var glop = 'script'
 var glap = 'yip.su'
