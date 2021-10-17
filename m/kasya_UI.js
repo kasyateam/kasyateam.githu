500489d024ecbb4f361f022c4677fbf7bf51f4a3
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-10-18 03:55:16 +0600

    Update kasya_UI.js

--- m/kasya_UI.js
+++ m/kasya_UI.js
@@ -1,6 +1,6 @@
 //Уведомления
 var info_h = 'Обновление K.Feed'
-var info_t = 'Демонстрация Kasya UI третьего поколения'
+var info_t = 'Демонстрация Kasya UI третьего поколения<br>Вам скоро будет необхожимо обновить приложение'
 
 
 var post = '<div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс HTML</p></div><a id="button" href="https://kasyateam.github.io/m/html/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс CSS</p></div><a id="button" href="https://kasyateam.github.io/m/css/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс JavaScript</p></div><a id="button" href="https://kasyateam.github.io/m/js/start.html">Перейти</a></div>'
